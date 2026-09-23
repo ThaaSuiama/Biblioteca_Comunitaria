ecff711cc5b3b05c4671f7fe11b4d3d2fd7df795
Thainá Suiama <thaasuiama@hotmail.com>
2026-09-23 14:19:14 -0300

    Configurar conexão com banco Aiven

--- database.py
+++ database.py
@@ -2,17 +2,18 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 
 def conectar():
     banco = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
-
     return banco
 
 
